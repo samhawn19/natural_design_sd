b3d2188c2695972e70cb07a19a616bdfa3256d4f
samhawn19 <samhawn19@gmail.com>
2016-01-15 14:13:51 -0800

    Very first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# natural_design_sd
---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("nav01").innerHTML = 
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='contactus.html'>Contact us</a></li>" +
+"</ul>";
+
+// document.getElementById("footer").innerHTML = 
+// "<ul id='footer'>" +
+// "<li><a href='index.html'>Home</a></li>" +
+// "<li><a href='about.html'>About</a></li>" +
+// "<li><a href='contactus.html'>Contact us</a></li>" +
+// "</ul>";
+
